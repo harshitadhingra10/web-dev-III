12b92bd63e8756e840203a43f3229297e3921225
Harshita Dhingra <harshitadhinra7@gmail.com>
2026-08-06 13:38:48 +0530

    lecture 3 and lecture 4

---
+++ lecture 4/index.js
@@ -0,0 +1,6 @@
+// const math = require("./math")
+
+// console.log(math.add(10,5))
+import {sub} from "./math.js"
+
+console.log(sub(15,10))
---
+++ lecture 4/math.js
@@ -0,0 +1,13 @@
+// function add(a,b){
+//     return a+b;
+
+// }
+
+// module.exports = {add}
+
+ export function sub(a,b){
+return a-b;
+
+}
+// module.exports = {sub}
+
